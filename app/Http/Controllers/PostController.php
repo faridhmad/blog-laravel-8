87d468419e4eba4b0062939e7e45bcbfcf585924
Fauzan Iliya Khalid <iliya@Fauzans-MacBook-Air.local>
2022-05-24 18:03:48 +0800

    feat: add search, filtering, pagination, forms authentication, comment, newsletter, email and admin section

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Validation\Rule;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(
+                        request(['search', 'category', 'author'])
+                    )->paginate(5)->withQueryString()
+        ]);
+    }
+
+    public function show(Post $post)
+    {
+        //dd($post);
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
+}
